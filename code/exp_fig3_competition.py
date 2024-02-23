05aaee0e88ed74262480dd07260ab1b2035abe8c
Laura Naumann <laurabella.naumann@gmail.com>
2024-02-23 10:20:29 +0100

    clean up plotting code, rename plotting functions, add supp

--- code/exp_fig3_competition.py
+++ code/exp_fig3_competition.py
@@ -20,15 +20,17 @@ cPC, cPV, cSOM, cNDNF, cVIP, cpi = get_model_colours()
 DPI = 300
 
 
-def exp_fig3top_layer_specificity(dur=1500, dt=1, w_hetero=False, mean_pop=True, noise=0.0, pre_inh=True, save=False):
+def exp_fig3top_vary_NDNF_input(dur=1500, dt=1, w_hetero=False, mean_pop=True, noise=0.0, pre_inh=True, target_ND=False, save=False):
     """
-    Experiment3: Vary input to NDNF interneurons, monitor NDNF- and SOM-mediated dendritic inhibition and their activity.
+    Vary input to NDNF interneurons, monitor NDNF- and SOM-mediated dendritic inhibition and their activity.
 
     Parameters
     - dur: duration of experiment (ms)
     - dt: integration time step (ms)
+    - w_hetero: whether to add heterogeneity to weight matrices
+    - mean_pop: if true, simulate only one neuron (mean) per population
     - noise: level of white noise added to neural activity
-    - flag_w_hetero: whether to add heterogeneity to weight matrices
+    - pre_inh: whether to include presynaptic inhibition
     - save: if it's a string, name of the saved file, else if False nothing is saved
     """
 
@@ -37,13 +39,6 @@ def exp_fig3top_layer_specificity(dur=1500, dt=1, w_hetero=False, mean_pop=True,
 
     # get default parameters
     N_cells, w_mean, conn_prob, bg_inputs, taus = mb.get_default_params(flag_mean_pop=mean_pop)
-
-    # TODO: remove
-    # w_mean.update(dict(SV=0.4, PV=0.2, PN=0.3, VN=0.2, DE=0.2))
-    # w_mean.update(dict(DN=0.4))
-    # w_mean['NS'] = 1.5
-
-    # mb.plot_w_mean(w_mean)
 
     # array for varying NDNF input
     ndnf_input = np.arange(-1, 1, 0.05)
@@ -54,7 +49,6 @@ def exp_fig3top_layer_specificity(dur=1500, dt=1, w_hetero=False, mean_pop=True,
     rN_inh_record = np.zeros(ninput)
     rS_record = np.zeros((ninput, N_cells['S']))
     rN_record = np.zeros((ninput, N_cells['N']))
-    # rV_record = np.zeros((ninput, N_cells['V'])) # TODO: remove
     cGABA_record = np.zeros(ninput)
     p_record = np.zeros(ninput)
 
@@ -64,73 +58,65 @@ def exp_fig3top_layer_specificity(dur=1500, dt=1, w_hetero=False, mean_pop=True,
         xFF = get_null_ff_input_arrays(nt, N_cells)
         xFF['N'][:, :] = I_activate
 
+        if target_ND:
+            flag_p_on_DN = True
+            # w_mean['DN'] = 0.6
+
         # instantiate and run model
         model = mb.NetworkModel(N_cells, w_mean, conn_prob, taus, bg_inputs, wED=1, flag_w_hetero=w_hetero,
-                                flag_pre_inh=pre_inh)
+                                flag_pre_inh=pre_inh, flag_p_on_DN=flag_p_on_DN)
         t, rE, rD, rS, rN, rP, rV, p, cGABA, other = model.run(dur, xFF, dt=dt, init_noise=0, monitor_dend_inh=True,
                                                                noise=noise)
-        print('bg inputs:', model.Xbg)
-        # TODO: add init_noise?
 
         # save stuff
         rS_record[i] = rS[-1]
         rN_record[i] = rN[-1]
-        # rV_record[i] = rV[-1] #TODO: remove
         rS_inh_record[i] = np.mean(np.array(other['dend_inh_SOM'][-1]))
         rN_inh_record[i] = np.mean(np.array(other['dend_inh_NDNF'][-1]))
         cGABA_record[i] = np.mean(cGABA[-1])
         p_record[i] = p[-1]
+            
 
-    # plotting
+    # Plotting
+    # --------
     dpi = 300 if save else DPI
     fig, ax = plt.subplots(2, 1, figsize=(1.6, 2.5), dpi=dpi, gridspec_kw={'left': 0.22, 'bottom': 0.15, 'top': 0.95,
                                                                            'right': 0.95, 'hspace': 0.2,
                                                                            'height_ratios': [1, 1]}, sharex=True)
+    # plot SOM and NDNF activity
+    ax[0].plot(ndnf_input, np.mean(rS_record, axis=1), color=cSOM, lw=lw)
+    ax[0].plot(ndnf_input, np.mean(rN_record, axis=1), color=cNDNF, lw=lw)
+    ax[0].legend(['SOM', 'NDNF'], frameon=False, handlelength=1, loc=(0.05, 0.6), fontsize=8)
+    # plot SOM and NDNF dendritic inhibition and sum (i.e. total dendritic inhibition)
     ax[1].plot(ndnf_input, rS_inh_record, c=cSOM, ls='--', lw=lw)
     ax[1].plot(ndnf_input, rN_inh_record, c=cNDNF, ls='--', lw=lw)
-    ax[1].plot(ndnf_input, rS_inh_record+rN_inh_record, c='#978991', ls='-', lw=lw, zorder=-1)
-    rSmu, rSstd = np.mean(rS_record, axis=1), np.std(rS_record, axis=1)
-    rNmu, rNstd = np.mean(rN_record, axis=1), np.std(rN_record, axis=1)
-    ax[0].plot(ndnf_input, rSmu, color=cSOM, lw=lw)
-    ax[0].plot(ndnf_input, rNmu, color=cNDNF, lw=lw)
-    # ax[0].plot(ndnf_input, np.mean(rV_record, axis=1), color=cVIP, lw=lw)  # TODO:remove
-    ax[0].legend(['SOM', 'NDNF'], frameon=False, handlelength=1, loc=(0.05, 0.6), fontsize=8)
-    # ax[1].fill_between(ndnf_input, rSmu-rSstd, rSmu+rSstd, color=cSOM, alpha=0.5)
-    # ax[1].fill_between(ndnf_input, rNmu-rNstd, rNmu+rNstd, color=cNDNF, alpha=0.5)
-
+    if not target_ND:
+        ax[1].plot(ndnf_input, rS_inh_record+rN_inh_record, c='#978991', ls='-', lw=lw, zorder=-1)
     # labels etc
+    ax[0].set(ylabel='activity (au)', xlim=[-1, 1], ylim=[-0.1, 2.5], yticks=[0, 1, 2])
     ax[1].set(ylabel='dend. inh. (au)', ylim=[-0.05, 1.1], yticks=[0, 1], xlabel=r'$\Delta$ NDNF input')
-    ax[0].set(ylabel='activity (au)', xlim=[-1, 1], ylim=[-0.1, 2.5],
-              yticks=[0, 1, 2])
 
-    # TODO: remove
-    # fig2, ax2 = plt.subplots(2, 1, figsize=(2.1, 2.8), dpi=dpi, gridspec_kw={'left': 0.25, 'bottom': 0.15, 'top': 0.95,
-    #                                                                        'right': 0.95,
-    #                                                                        'height_ratios': [1, 1]}, sharex=True)
-    # ax2[0].plot(ndnf_input, cGABA_record, c=cpi)
-    # ax2[1].plot(ndnf_input, p_record, c=cpi)
-    # ax2[0].set(ylabel='GABA conc. (au)')
-    # ax2[1].set(ylabel='release p', xlabel='input to NDNF (au)')
-
-    # saving
+    # Saving
+    # ------
     if save:
-        savename = '../results/figs/Naumann23_draft1/exp1-3_layer-specificity.pdf'
+        savename = '../results/figs/Naumann23_draft1/exp1-3_layer-specificity.pdf' if not isinstance(save, str) else save
         if not pre_inh:
             savename = savename.replace('.pdf', '_no-preinh.pdf')
         fig.savefig(savename, dpi=300)
-        # fig2.savefig('../results/figs/cosyne-collection/exp1-3_layer-specificity_GABA', dpi=300)
         plt.close(fig)
 
 
 def exp_fig3bottom_total_dendritic_inhibition(dur=1500, dt=1, w_hetero=False, mean_pop=True, noise=0.0, pre_inh=True, save=False):
     """
-    Experiment3b: Vary input to NDNF interneurons and NDNF->dendrite weight, check how this affects total dendritic inhibition.
+    Vary input to NDNF interneurons and NDNF->dendrite weight, check how this affects total dendritic inhibition.
 
     Parameters
     - dur: duration of experiment (ms)
     - dt: integration time step (ms)
+    - w_hetero: whether to add heterogeneity to weight matrices
+    - mean_pop: if true, simulate only one neuron (mean) per population
     - noise: level of white noise added to neural activity
-    - flag_w_hetero: whether to add heterogeneity to weight matrices
+    - pre_inh: whether to include presynaptic inhibition
     - save: if it's a string, name of the saved file, else if False nothing is saved
     """
 
@@ -144,9 +130,6 @@ def exp_fig3bottom_total_dendritic_inhibition(dur=1500, dt=1, w_hetero=False, me
     ndnf_input = np.arange(-1, 1.05, 0.1)
     weightsDN = np.arange(0, 0.9, 0.2)
 
-    # TODO: remove
-    # w_mean.update(dict(SV=0.4, PV=0.2, PN=0.3, VN=0.2, DE=0.2))
-
     # empty arrays for recording stuff
     rS_inh_record = np.zeros((len(ndnf_input), len(weightsDN)))
     rN_inh_record = np.zeros((len(ndnf_input), len(weightsDN)))
@@ -155,21 +138,18 @@ def exp_fig3bottom_total_dendritic_inhibition(dur=1500, dt=1, w_hetero=False, me
     dpi = 300 if save else DPI
     fig, ax = plt.subplots(1, 1, figsize=(1.6, 1.5), dpi=dpi, gridspec_kw={'left': 0.22, 'bottom': 0.25, 'top': 0.95,
                                                                            'right': 0.95}, sharex=True)
-    # fig2, ax2 = plt.subplots(1, 1, figsize=(1.6, 1.5), dpi=dpi, gridspec_kw={'left': 0.22, 'bottom': 0.25, 'top': 0.95,
-                                                                        #    'right': 0.95}, sharex=True)
     cols = sns.color_palette(f"blend:{cSOM},{cNDNF}", n_colors=len(weightsDN))
 
+    # loop over NDNF input and NDNF->dendrite weight, simulate, record and plot
     for j, wDN in enumerate(weightsDN):
 
         w_mean['DN'] = wDN
         print(f"NDNF/SOM dendritic inh: {w_mean['DN']/w_mean['DS']:1.2f}")
 
         # instantiate and run model
-        # bg_inputs = {'E': 0.5, 'D': 1.9, 'N': 1.9, 'S': 0.7, 'P': 1.7, 'V': 1.5}
-        bg_inputs = {'E': 0.5, 'D': 1.7, 'N': 1.9, 'S': 0.6, 'P': 1.4, 'V': 1.4}
+        # bg_inputs = {'E': 0.5, 'D': 1.7, 'N': 1.9, 'S': 0.6, 'P': 1.4, 'V': 1.4} # fix background inputs to default
         model = mb.NetworkModel(N_cells, w_mean, conn_prob, taus, bg_inputs, wED=1, flag_w_hetero=w_hetero,
                                 flag_pre_inh=pre_inh)
-        
 
         for i, I_activate in enumerate(ndnf_input):
 
@@ -177,23 +157,22 @@ def exp_fig3bottom_total_dendritic_inhibition(dur=1500, dt=1, w_hetero=False, me
             xFF = get_null_ff_input_arrays(nt, N_cells)
             xFF['N'][:, :] = I_activate
 
+            # simulate
             t, rE, rD, rS, rN, rP, rV, p, cGABA, other = model.run(dur, xFF, dt=dt, init_noise=0, monitor_dend_inh=True,
-                                                               noise=noise, calc_bg_input=False)
-            # TODO: add init_noise?
+                                                                   noise=noise, calc_bg_input=True)
 
-            # save stuff
+            # save dendritic inhibition
             rS_inh_record[i, j] = np.mean(np.array(other['dend_inh_SOM'][-1]))
             rN_inh_record[i, j] = np.mean(np.array(other['dend_inh_NDNF'][-1]))
 
+        # plot
         ax.plot(ndnf_input, rS_inh_record[:, j]+rN_inh_record[:, j], c=cols[j], ls='-', label=f"{wDN/w_mean['DS']:1.1f}", lw=lw)
     
+        # background inputs are recomputed for each weight parametrisation
         print(model.Xbg)
 
-    ax.set(xlabel=r'$\Delta$ NDNF input', xticks=[-1, 0, 1], xlim=[-1, 1], ylim=[0, 2], yticks=[0, 1, 2], ylabel=r'$\Sigma$ dend. inh.')
-    # ax.legend(loc=(1.01, 0.2), frameon=False, handlelength=1, title='N/S -> D')
-
-    #TODO: remove
-    # ax2.pcolormesh(ndnf_input, weightsDN, (rS_inh_record+rN_inh_record).T, cmap='coolwarm_r', vmin=0, vmax=2)
+    # labels
+    ax.set(xlabel=r'$\Delta$ NDNF input', xticks=[-1, 0, 1], xlim=[-1, 1], ylim=[-0.1, 2], yticks=[0, 1, 2], ylabel=r'$\Sigma$ dend. inh.')
 
     # saving
     if save:
@@ -523,13 +502,18 @@ if __name__ in "__main__":
 
     SAVE = False
 
-    # Fig 3 (top): Layer-specificity of NDNF control (with & without pre inh)
-    exp_fig3top_layer_specificity(mean_pop=False, w_hetero=True, noise=0.1, pre_inh=True, save=SAVE)
-    exp_fig3top_layer_specificity(mean_pop=False, w_hetero=True, noise=0.1, pre_inh=False, save=SAVE)
+    # # Fig 3 (top): Layer-specificity of NDNF control (with & without pre inh)
+    # exp_fig3top_vary_NDNF_input(pre_inh=True, mean_pop=False, w_hetero=True, noise=0.1, save=SAVE)
+    # exp_fig3top_vary_NDNF_input(pre_inh=False, mean_pop=False, w_hetero=True, noise=0.1, save=SAVE)
 
-    # Fig 3 (bottom): total dendritic inhibiion
-    exp_fig3bottom_total_dendritic_inhibition(pre_inh=True, save=SAVE, w_hetero=True, mean_pop=False, noise=0.1)
-    exp_fig3bottom_total_dendritic_inhibition(pre_inh=False, save=SAVE, w_hetero=True, mean_pop=False, noise=0.1)
+    # # Fig 3 (bottom): total dendritic inhibiion
+    # exp_fig3bottom_total_dendritic_inhibition(pre_inh=True, mean_pop=False, w_hetero=True, noise=0.1, save=SAVE)
+    # exp_fig3bottom_total_dendritic_inhibition(pre_inh=False, mean_pop=False, w_hetero=True, noise=0.1, save=SAVE)
+
+    # Fig 3 supp: pre inh targets NDNF-dendrite synapses
+    exp_fig3top_vary_NDNF_input(pre_inh=True, mean_pop=False, w_hetero=True, noise=0.1, target_ND=True, dur=3000,
+                                save='../results/figs/Naumann23_draft1/supps/fig34_supp1a.pdf')
+
 
 
     # --- OLD --- 
